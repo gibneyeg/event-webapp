6b4cb28f2e1baea7f3f63939e20b5f66b5520e0d
Eve Gibney <gibney.eg@gmail.com>
2025-02-26 10:30:21 +0000

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Test</h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <h1>Event Page</h1>
+      <p>Event page !</p>
+    </div>
+  );
+}
+
+export default HomePage;
