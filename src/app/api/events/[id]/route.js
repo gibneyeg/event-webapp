ded58462f51fe01aef0040c8d40effdd828d09e4
Eve <gibney.eg@gmail.com>
2025-03-23 16:58:39 +0000

    had lib folder in wrong spot

--- src/app/api/events/[id]/route.js
+++ src/app/api/events/[id]/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import prisma from '@/app/lib/prisma';
+import prisma from '@/lib/prisma';
 
 // GET /api/events/[id] - Get a single event
 export async function GET(request, { params }) {
