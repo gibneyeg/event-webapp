26982d499d6ced33344d9379682f3f9ebdca8115
wiznob <wisbob27@gmail.com>
2025-03-26 12:07:58 +0000

    Added header and background to homepage

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,54 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
+<div>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="#">
+      <Image src="/avatars/logo.png" alt="Logo" width={40} height={40} />
+    </a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mx-auto">
+        <li className="nav-item">
+          <a className= "btn btn-secondary" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="btn btn-secondary" href="#">Events</a>
+        </li>
+        <li className="nav-item">
+          <a className="btn btn-secondary" href="#">Rainbow 6</a>
+        </li>
+      </ul>
+
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item active">
+          <a className="nav-link" href="#">Sign Up <span className="sr-only"></span></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Sign In</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</div>
       <div style={{ textAlign: 'center', margin: '40px 0' }}>
-        <h1 style={{ fontSize: '36px', fontWeight: 'bold', marginBottom: '24px' }}>Welcome to Event Creator</h1>
+        <h1 className="text-danger" style={{ fontSize: '36px', fontWeight: 'bold', marginBottom: '24px' }}>Welcome to Event Creator</h1>
         <p style={{ fontSize: '20px', marginBottom: '32px' }}>Create and manage your events in one place</p>
         
         <Link href="/events/new">
-          <button style={{ padding: '12px 24px', fontSize: '18px', borderRadius: '24px' }}>
+          <button className = "btn btn-info" style={{ padding: '12px 24px', fontSize: '18px', borderRadius: '24px' }}>
             Create New Event
           </button>
         </Link>
         <Link href="/events/new">
-          <button style={{ padding: '12px 24px', fontSize: '18px', borderRadius: '24px' }}>
-            login
+          <button className = "btn btn-info" style={{ padding: '12px 24px', fontSize: '18px', borderRadius: '24px' }}>
+            view events
           </button>
         </Link>
       </div>
