690f8269ee90555af2cc1abfaaae91e1de74ff74
Eve <gibney.eg@gmail.com>
2025-04-03 19:49:01 +0100

    moved e2e test location because of bug and add simple github actions file

--- playwright.config.js
+++ playwright.config.js
@@ -1,9 +1,7 @@
-// playwright.config.js
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests',
-  fullyParallel: true,
+testDir: './e2e/specs',  fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
