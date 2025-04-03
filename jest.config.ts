064109b723fca59c65c36b6c90e3a66fd40b7d1d
wiznob <wisbob27@gmail.com>
2025-04-03 17:13:23 +0100

    improved signup test and added event form test

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ const nextJest = require('next/jest');
 const createJestConfig = nextJest({
   dir: './',
 });
-
+process.env.TZ = 'UTC';
 const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
